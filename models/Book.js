4ccd4f208cf0d3c91242e9fec043d59e24f2dada
SergeyEzhin <ezhin@mail.ru>
2022-02-13 17:22:23 +0400

    hw-library1

---
+++ models/Book.js
@@ -0,0 +1,14 @@
+class Todo {
+    constructor(id = "", title = "", description = "", 
+    authors = "", favorite = "", fileCover = "", fileName = "") {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.authors = authors;
+        this.favorite = favorite;
+        this.fileCover = fileCover;
+        this.fileName = fileName;
+    }
+}
+
+module.exports = Todo;
